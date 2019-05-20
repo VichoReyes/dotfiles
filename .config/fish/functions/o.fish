6d6bad503704e8066a6edbbede5b2b46703c334b
Vicente Reyes <vreyesvaldivieso@gmail.com>
2019-05-20 17:25:50 -0400

    switch to fzf instead of fzy
    because it's more coherent with ivy's behaviour

--- .config/fish/functions/o.fish
+++ .config/fish/functions/o.fish
@@ -1,8 +1,8 @@
 # Defined in - @ line 1
-function o --description 'alias o nohup xdg-open (ls -a | fzy) >/dev/null'
+function o --description 'alias o nohup xdg-open (ls -a | fzf) >/dev/null'
 	if count $argv > /dev/null
 		nohup xdg-open $argv >/dev/null;
 	else
-		nohup xdg-open (ls -a | fzy) >/dev/null;
+		nohup xdg-open (ls -a | fzf) >/dev/null;
 	end
 end
--- .config/fish/functions/vim2conf.fish
+++ .config/fish/functions/vim2conf.fish
@@ -1,3 +1,3 @@
 function vim2conf
-	vim (find ~/.config/ -maxdepth 4 '(' -name .git -o -name .stack-work -o -name cache -o -name .cache ')' -prune -o -type f -print | fzy)
+	vim (find ~/.config/ -maxdepth 4 '(' -name .git -o -name .stack-work -o -name cache -o -name .cache ')' -prune -o -type f -print | fzf)
 end
