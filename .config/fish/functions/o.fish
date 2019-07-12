d31bc37c11e71b7e3b0e8db13ef21eaaa427b965
Vicente Reyes <vreyesvaldivieso@gmail.com>
2019-07-12 15:17:18 -0400

    improve open function

--- .config/fish/functions/o.fish
+++ .config/fish/functions/o.fish
@@ -1,7 +1,9 @@
 # Defined in - @ line 1
-function o --description 'alias o nohup xdg-open (ls -a | fzf) >/dev/null'
+function o --description 'open in default program'
 	if count $argv > /dev/null
-		nohup xdg-open $argv >/dev/null;
+		for file in $argv
+			nohup xdg-open $file >/dev/null;
+		end
 	else
 		nohup xdg-open (ls -a | fzf) >/dev/null;
 	end
