f4df1ece710ded5396b4089ad2833d567e8f93fe
Vicente Reyes <vreyesvaldivieso@gmail.com>
2019-07-23 21:58:40 -0400

    improve "hide" fish function

--- .config/fish/functions/hide.fish
+++ .config/fish/functions/hide.fish
@@ -1,3 +1,7 @@
 function hide --argument name
-	grep -li "^Name=$name" /usr/share/applications/* | hideMenu
+	set -l toHide (egrep -li "^Name(\[.*\])?=$name" /usr/share/applications/*)
+	for file in $toHide
+		set -l stmt 's/\[Desktop Entry\]/[Desktop Entry]\nNoDisplay=true/'
+		sed $stmt $file >$HOME/.local/share/applications/(basename $file)
+	end
 end
--- .config/fish/functions/mopi.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function mopi --description 'alias mopi=http -bj POST "http://localhost:6680/mopidy/rpc" jsonrpc=2.0 params:="{}" id:=1'
-	http -bj POST "http://localhost:6680/mopidy/rpc" jsonrpc=2.0 params:="{}" id:=1 $argv;
-end
