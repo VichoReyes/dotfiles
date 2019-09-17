deb84763bda829a8ffe108cff0cc48afd3f0fb77
Vicente Reyes <vreyesvaldivieso@gmail.com>
2019-09-17 16:42:18 -0300

    update

--- .config/fish/functions/clone.fish
+++
@@ -1,6 +0,0 @@
-# Defined in - @ line 0
-function clone --description 'alias clone=go get -d && cd'
-	set -l path (echo $argv | sed -E 's/^[a-z]+:\/\///g')
-go get -d $path;
-cd $GOPATH/src/$path;
-end
